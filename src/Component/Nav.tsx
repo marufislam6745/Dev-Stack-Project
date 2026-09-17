f6bd6b181c3094ffcf47fbf1e4e25637553a9010
marufislam6745 <marufislam6745@gmail.com>
2026-09-17 17:12:50 +0600

    change commit

--- src/Component/Nav.tsx
+++ src/Component/Nav.tsx
@@ -1,26 +1,52 @@
+import { useState } from "react"
 import logo from "../assets/logo.png"
 
 
 export function Nav() {
-   
+    const [menu, setMenu] = useState<boolean>(false)
+
     return (
-        <div className="w-300 mx-auto py-4 flex justify-between items-center sticky top-0 bg-white">
-            <div>
-                <img src={logo} alt="photo" />
+        <div>
+            <div className="w-300 mx-auto py-4 flex justify-between items-center sticky top-0 bg-white">
+                <button
+                    onClick={() => setMenu(!menu)}
+                    className="sm:hidden flex flex-col gap-1.5"
+                >
+                    <span className="w-8 h-1 bg-gray-500 rounded"></span>
+                    <span className="w-8 h-1 bg-gray-500 rounded"></span>
+                    <span className="w-8 h-1 bg-gray-500 rounded"></span>
+                    <span className="w-8 h-1 bg-gray-500 rounded"></span>
+                    <span className="w-8 h-1 bg-gray-500 rounded"></span>
+                </button>
+
+                <div>
+                    <img src={logo} alt="photo" />
+                </div>
+                <div>
+                    <ul className="hidden sm:flex justify-center items-center gap-5">
+                        <li className="text-sm text-gray-600 hover:text-fuchsia-700 cursor-pointer">Home</li>
+                        <li className="text-sm text-gray-600 hover:text-fuchsia-700 cursor-pointer">Technology</li>
+                        <li className="text-sm text-gray-600 hover:text-fuchsia-700 cursor-pointer">Project</li>
+                        <li className="text-sm text-gray-600 hover:text-fuchsia-700 cursor-pointer">About</li>
+                        <li className="text-sm text-gray-600 hover:text-fuchsia-700 cursor-pointer">Contact</li>
+                    </ul>
+                </div>
+                <div className="flex items-center gap-2 sm:4">
+                    <button className="py-2 px-4 rounded-xl sm:text-base">Sign in</button>
+                    <button className="bg-linear-to-r from-fuchsia-600 to-blue-500 py-2 px-4 rounded-xl text-gray-50 sm:text-base">Sign up</button>
+                </div>
             </div>
-            <div>
-                <ul className="flex justify-center items-center gap-5">
-                    <li className="text-sm text-gray-600 hover:text-fuchsia-700 cursor-pointer">Home</li>
-                    <li className="text-sm text-gray-600 hover:text-fuchsia-700 cursor-pointer">Technology</li>
-                    <li className="text-sm text-gray-600 hover:text-fuchsia-700 cursor-pointer">Project</li>
-                    <li className="text-sm text-gray-600 hover:text-fuchsia-700 cursor-pointer">About</li>
-                    <li className="text-sm text-gray-600 hover:text-fuchsia-700 cursor-pointer">Contact</li>
-                </ul>
-            </div>
-            <div>
-                <button className="hover:bg-fuchsia-200 py-2 px-4 rounded-xl">Sign in</button>
-                <button className="bg-linear-to-r from-fuchsia-600 to-blue-500 py-2 px-4 ml-2 rounded-xl text-gray-50">Sign up</button>
-            </div>
+            {menu && (
+                <div className="sm:hidden mt-5 pt-4">
+                    <ul className="flex flex-col gap-4 text-center">
+                        <li className="text-gray-600 hover:text-pink-600 cursor-pointer">Home</li>
+                        <li className="text-gray-600 hover:text-pink-600 cursor-pointer">Technology</li>
+                        <li className="text-gray-600 hover:text-pink-600 cursor-pointer">Project</li>
+                        <li className="text-gray-600 hover:text-pink-600 cursor-pointer">About</li>
+                        <li className="text-gray-600 hover:text-pink-600 cursor-pointer">Contact</li>
+                    </ul>
+                </div>
+            )}
         </div>
     )
 }
